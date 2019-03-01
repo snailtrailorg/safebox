67a449c1102484c684557ac84be42115d2324380
michael <pillarwang@hotmail.com>
2019-03-01 17:59:00 +0800

    mod

--- app/src/main/java/org/snailtrail/safebox/MainActivity.java
+++ app/src/main/java/org/snailtrail/safebox/MainActivity.java
@@ -13,11 +13,14 @@ import java.lang.ref.WeakReference;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -84,7 +87,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         RecyclerView recyclerView = findViewById(R.id.safe_recycle_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        m_safeRecycleAdapter = new SafeRecycleAdapter(this);
+        m_safeRecycleAdapter = new SafeRecycleAdapter(this, recyclerView);
         recyclerView.setAdapter(m_safeRecycleAdapter);
 
         m_isUserSignedIn = false;
